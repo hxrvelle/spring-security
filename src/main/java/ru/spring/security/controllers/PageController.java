1c5a86df3dbef1749502197ac9bf8e9c39649f23
Darya Rozhnova <hxrvelle@gmail.com>
2023-10-09 21:40:55 +0300

    --upd

---
+++ src/main/java/ru/spring/security/controllers/PageController.java
@@ -0,0 +1,22 @@
+package ru.spring.security.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
